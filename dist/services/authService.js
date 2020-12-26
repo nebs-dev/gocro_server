34fa7fe64209ce3d7c715f559df4e9d62b2cd4f2
Nebojsa Stojanovic <nebojsa@2amigos.us>
2020-12-26 18:37:54 +0100

    removed vscode and dist folders

--- dist/services/authService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAuthenticated = void 0;
-function isAuthenticated(req) {
-    return true;
-}
-exports.isAuthenticated = isAuthenticated;
